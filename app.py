70cc6d839a3a153096200d6b63eb574f8aa95123
noshinrahman11 <noshinrahman11@gmail.com>
2025-03-04 13:43:12 -0500

    window mainloop

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 from tkinter import Tk, filedialog
 
 
@@ -6,18 +6,21 @@ window = Tk()
 
 window.title("Task Management System")
 window.geometry('350x200')
+lbl = Label(window, text = "Hello", font=("Arial Bold", 50))
+lbl.grid(column=0, row=0)
 
+btn = Button()
 
+window.mainloop()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('dashboard'))
+# @app.route('/')
+# def index():
+#     # if current_user.is_authenticated:
+#     #     return redirect(url_for('dashboard'))
     
-    return render_template('index.html')
+#     return render_template('index.html')
 
-print('Hi')
-
+# print('Hi')
 

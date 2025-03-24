d49c1d0409539773f1294868802328f7560ef2b5
noshinrahman11 <noshinrahman11@gmail.com>
2025-03-23 23:28:06 -0400

    sender credentials

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def create_app(config_class=Config):
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') # aebn jexs dokr whwb
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
     login_manager.init_app(app)

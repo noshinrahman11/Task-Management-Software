f3947495bba2550b88f5deb8e90951c11c892357
noshinrahman11 <noshinrahman11@gmail.com>
2025-04-13 18:28:42 -0400

    moved calendar sync route stuff into features_routes.py

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ with app.app_context():
 
 @app.errorhandler(401)
 def unauthorized(e):
-    return render_template("login.html")
+    return render_template("404.html")
     
 @app.errorhandler(404)
 def not_found(e):
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('404.html'))
 
 if __name__ == "__main__":
     # app.run(host='localhost', port=5000, debug=True)

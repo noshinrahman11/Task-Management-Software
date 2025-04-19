092dcb54c6b8986d212a16e8425a6b2ede94c45f
noshinrahman11 <noshinrahman11@gmail.com>
2025-04-19 01:23:42 -0400

    changed app.run port

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ with app.app_context():
 #     return redirect(url_for('auth.login'))
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    # app.run(host='localhost', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
     # def run_flask():
     #     print ("Starting Flask app...")
